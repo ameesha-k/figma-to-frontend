1e672ea4dbc831c5d3a9895d9337850f160719f6
ameesha-k <ameeshakulkarni@utexas.edu>
2021-08-03 18:44:11 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components"
+const App = () => {
+  return (
+    <div>
+     
+      
+    </div>
+  )
+     
+ 
+}; 
+const Container = styled.div
+const Wrapper = styled.div
+export default App; 
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Main = () => {
+  return (
+    <div>
+      <h1> main</h1>
+    </div>
+  )
+     
+ 
+}; 
+const Container = styled.div
+
+export default Main; 
